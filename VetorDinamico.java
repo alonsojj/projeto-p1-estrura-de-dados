57af263044cca824f66ce2e7d5a6b6d44ffcd1b1
alonsojj <juan13032007@gmail.com>
2026-04-21 03:03:03 -0300

    docs: add team member names to class comments

--- VetorDinamico.java
+++ VetorDinamico.java
@@ -1,3 +1,11 @@
+/*
+ * Integrantes do grupo:
+ * - Isabella Silva e Santos RA: 1430482513004
+ * - Gabriel Vicente Bernardinelli RA: 1430482513038
+ * - Jorge Luiz Barbosa da Silva RA: 1430482411013
+ * - Juan dos Santos Alonso RA: 1430482513009
+ * - Nicole Lira Nova Cardoso RA: 1430482513043
+ */
 
 public class VetorDinamico {
   private Processo[] vetor;
@@ -81,3 +89,4 @@ public class VetorDinamico {
     return capacidade;
   }
 }
+
